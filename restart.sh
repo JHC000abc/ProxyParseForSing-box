efe50ecbd7fc4f2ad0b26866f201cc2b2e8af32e
developer <jhc000abc@g,ain.com>
2025-08-20 14:22:30 +0800

    更新restart脚本代理配置

--- restart.sh
+++ restart.sh
@@ -4,13 +4,16 @@
 # 设置默认值
 default_url="https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/singbox/config.json"
 default_port=1080
+proxy=""
 
 
 url="${1:-$default_url}"
 port="${2:-$default_port}"
+proxy="${3:-$proxy}"
 
 echo "url=$url"
 echo "port=$port"
+echo "proxy=$proxy"
 
 # 启动前准备
 cd /etc/sing-box
@@ -20,10 +23,16 @@ pkill -f sing-box
 # 下载订阅链接内容
 rm config.json
 touch config.json
-curl -o ./config.json $url
+
+if [ ! -z "$proxy" ]; then
+  echo "使用代理: $proxy"
+  curl -o ./config.json $url -x "$proxy"
+else
+  echo "不使用代理"
+  curl -o ./config.json $url
+fi
 
 # 修改启动的代理端口
-# sed -i "s/1080/$port/g" ./config.json
 sed -i 's/"listen_port": [0-9]\+/"listen_port": '"$port"'/g' ./config.json
 
 #  启动sing-box
