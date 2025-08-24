32004c36177b4d3677a0427ed5edeb6192108115
jhc <JHC000abc@gmail.com>
2025-08-24 20:44:17 +0800

    更新20250823代理配置信息

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ PROXY_PORT = 10808
 
 # aiohttp 使用的代理(主要用于请求github等地址使用 程序中通过配置 self.infos 中的 proxy 确定是否启用)
 PROXIES_ASYNC = f"http://{PROXY_HOST}:{PROXY_PORT}"
-# PROXIES_ASYNC = None
+PROXIES_ASYNC = None
 
 
 SING_BOX_PATH = "./sing-box"
@@ -16,4 +16,4 @@ OUT_LISTEN_PORT = 1080
 TEST_LISTEN_PORT = 10800
 
 # 最大延迟(单位ms, 测速结果大于此值的不做保留)
-SPEED_LIMIT = 2000
+SPEED_LIMIT = 1000
