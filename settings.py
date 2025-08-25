01cd1d5d850bf2891299c4b089768fd20792b613
developer <jhc000abc@163.com>
2025-08-25 22:00:41 +0800

    2025.08.24

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ PROXIES_ASYNC = None
 SING_BOX_PATH = "plugins/sing-box"
 UPLOAD_TOOLS_FILE = "plugins/upload"
 TELEGRAM_TOOLS_FILE = "plugins/telegram"
-TRANS_PHONE_TOOLS_FILE = "plugins/telegram"
+TRANS_PHONE_TOOLS_FILE = "plugins/trans_phone"
 
 # 最后输出的订阅节点默认的测试端口
 OUT_LISTEN_PORT = 1080
