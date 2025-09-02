57fccaf95f89678486641001803ea275249f9162
developer <jhc000abc@gmain.com>
2025-09-02 15:15:31 +0800

    2025.09.02

--- utils/utils_test_speed.py
+++ utils/utils_test_speed.py
@@ -127,7 +127,7 @@ class TestSpeed:
                         if match_ip:
                             ip = match_ip.group(1)
                             flag_ip = True
-                        print("forbidden_area_map", forbidden_area_map)
+
                         for k, v in forbidden_area_map.items():
                             if k in msg2 or "400 Bad Reques" in msg2 or "Connection refused" in msg2:
                                 print(f"【禁止区域】 k:{k} server:{node_conf['server']} tag:{node_conf['tag']}")
