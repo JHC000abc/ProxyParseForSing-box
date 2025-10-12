38cb77242ddf8f6ebf94069cf0f94fddff0defa5
developer <jhc000abc@gmail.com>
2025-10-12 23:38:39 +0800

    2025.10.12

--- utils/utils_test_speed.py
+++ utils/utils_test_speed.py
@@ -114,9 +114,8 @@ class TestSpeed:
                 if match_speed:
                     msg3_all = ""
                     async for msg3, proc3 in self.cmd.run_cmd_async(cmd3):
-                        print("msg3--->", msg3)
                         msg3_all += msg3
-                    if "200" not in msg3_all:
+                    if msg3_all != "200":
                         print(f"节点：{node_conf} 访问 Gemini 不通")
                         return False, {}
 
